749f9bbbb42661164f23c3b2f3670a822feb895c
Adria <adria.blancafort@factorial.co>
2026-03-08 19:48:56 +0100

    first commit

---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+declare module '*.glsl' {
+	const content: string
+	export default content
+}
+
+declare const __AI_MOTION_VERSION__: string
---
+++ vite.lib.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+
+import { glslLoaderPlugin, replacePlugin } from './vite.plugins'
+
+export default defineConfig({
+	build: {
+		outDir: 'build',
+		lib: {
+			entry: 'src/Motion.ts',
+			name: 'Motion',
+			formats: ['es'],
+			fileName: () => 'Motion.js',
+		},
+		sourcemap: false,
+		minify: false,
+		emptyOutDir: true,
+	},
+	publicDir: false,
+	plugins: [
+		glslLoaderPlugin(),
+		replacePlugin({
+			__AI_MOTION_VERSION__: JSON.stringify(process.env.npm_package_version),
+		}),
+	],
+})
